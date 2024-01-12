c879d2634a61f1dcf3484331262a942c0ca2afa2
Ayushi Parmar <ayushi.parmar@1rivet.com>
2024-01-12 18:15:52 +0530

    feat: added friend date using lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,44 @@ import Button from "./components/Button";
 import FriendsList from "./components/FriendsList";
 import SplitBillForm from "./components/SplitBillForm";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [friends, setFriends] = useState(initialFriends);
+
   const handleShowFriend = () => {
     setShowForm((show) => !show);
+  };
+
+  const handleAddFriend = (friend) => {
+    setFriends((friends) => [...friends, friend]);
+    setShowForm(false);
   };
 
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {showForm && <AddFriendForm />}
+        <FriendsList friends={friends} />
+        {showForm && <AddFriendForm onAddFriend={handleAddFriend} />}
 
         <Button onClick={handleShowFriend}>
           {showForm ? "Close" : "Add friend"}
